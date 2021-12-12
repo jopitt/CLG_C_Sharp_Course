eb7e06f1b9b6afbee9e1db1e18811d72a248034d
Jo Pittendreigh <91508510+jopitt@users.noreply.github.com>
2021-12-12 15:59:22 +1000

    Add files via upload

---
+++ Lesson_1_Activity/Rhyming_Couplet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Rhyming_Couplet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("So now we’ve begun upon our road");
+            Console.WriteLine("Of learning C#, as we learn to code");
+            Console.WriteLine("Excited to learn this new mode!");
+        }
+    }
+}
